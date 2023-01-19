48ac544958b627d80975a4c3c7e238e1cf4dee08
Hiago Murilo <HiagoMuuu@gmail.com>
2023-01-18 22:02:07 -0300

    ajuste do parágrafo de apresentação

--- src/Components/blocks/About/index.tsx
+++ src/Components/blocks/About/index.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => {
             <img src="https://pbs.twimg.com/profile_images/1548517035289841669/BZNA6tJJ_400x400.jpg" alt="" />
             <styles.aboutText>
                 <h2>Sobre mim</h2>
-                <p>Olá, me chamo Hiago Paula, tenho 23 anos e atualmente sou estagiário em T.I. como Desenvolvedor Fullstack e estou cursando Engenharia da Computação na CESUPA. Sou apaixonado por tecnologia e sempre busco sempre evoluir em minhas habilidades e competências.</p>
+                <p>Olá, me chamo Hiago Paula, tenho 23 anos e atualmente sou estagiário em T.I. como Desenvolvedor Fullstack e estou cursando Engenharia da Computação na CESUPA. Sou apaixonado por tecnologia e sempre busco evoluir minhas habilidades e competências.</p>
             </styles.aboutText>
         </styles.about>
     )
